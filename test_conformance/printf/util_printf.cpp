39a5c31f9590b79788e9ad7a7e4b914ef265f479
Pranav Karale <pkarale@nvidia.com>
2025-04-22 10:21:55 +0530

    rolled back the update—#2094

--- test_conformance/printf/util_printf.cpp
+++ test_conformance/printf/util_printf.cpp
@@ -23,6 +23,7 @@
 
 // Helpers for generating runtime reference results
 static void intRefBuilder(printDataGenParameters&, char*, const size_t);
+static void longRefBuilder(printDataGenParameters&, char*, const size_t);
 static void halfRefBuilder(printDataGenParameters&, char* rResult,
                            const size_t);
 static void floatRefBuilder(printDataGenParameters&, char* rResult, const size_t);
@@ -157,33 +158,6 @@ std::vector<printDataGenParameters> printLongGenParameters = {
 
 };
 
-//--------------------------------------------------------
-
-//  Lookup table - [string]long-correct buffer             |
-
-//--------------------------------------------------------
-
-// The table below is used to accommodate differences in `printf` output when
-// using the `%ld` format specifier in 32-bit versus 64-bit compiled binaries
-
-std::vector<std::string> correctBufferLong = {
-
-    "10000000000",
-
-    "-10000000000   ",
-
-    "000010000000000",
-
-    "   -10000000000",
-
-    "+10000000000   ",
-
-    "000010000000000",
-
-    "-000010000000000"
-
-};
-
 //-----------------------------------------------
 
 // test case for long                             |
@@ -198,7 +172,7 @@ testCase testCaseLong = {
 
     printLongGenParameters,
 
-    NULL,
+    longRefBuilder,
 
     klong
 
@@ -1759,6 +1733,13 @@ static void intRefBuilder(printDataGenParameters& params, char* refResult, const
 {
     snprintf(refResult, refSize, params.genericFormats.front().c_str(),
              atoi(params.dataRepresentation));
+}
+
+static void longRefBuilder(printDataGenParameters& params, char* refResult,
+                           const size_t refSize)
+{
+    snprintf(refResult, refSize, params.genericFormats.front().c_str(),
+             atoll(params.dataRepresentation));
 }
 
 static void halfRefBuilder(printDataGenParameters& params, char* refResult,
